062bd67ab8e848eac2e8031878068b6704f16e21
henri gilsonpereira marques <134063038+GilsonPerera@users.noreply.github.com>
2024-11-27 15:29:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fazenda',
+    'Qual a raça de cavalo mais indicada para a lida com o gado?',
+    'Quarto de milha'
+)
+
+criaCartao(
+    'Fazenda',
+    'Qual o grão mais produzio do mundo?',
+    'Milho'
+)
+
+criaCartao(
+    'Fazenda',
+    'Qual a raça de gado mais utilizada no Brasil?',
+    'Nelore'
+)
+
+criaCartao(
+    'Fazenda',
+    'Quantos kg tem um arroba?',
+    '15'
+)
